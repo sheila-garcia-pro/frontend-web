d0cc64f71d3897ddb91efe0703f98587f249b6a5
JonatasCarvalhoSousa <jonatas.carvalho191098@outlook.com>
2025-04-16 19:30:48 -0300

    Remove files unnecessary

--- install-deps.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Instalando dependencias essenciais...
-call yarn add react-redux @reduxjs/toolkit redux-saga react-router-dom @mui/material @mui/icons-material @emotion/react @emotion/styled
-
-echo Instalacao concluida! 
