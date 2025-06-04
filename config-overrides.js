24aca78f61ca94d399d813f2d8d05c376c6baef2
JonatasCarvalhoSousa <jonatas.carvalho.191098@outlook.com>
2025-06-04 17:01:37 -0300

    Deploy master 2.6

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const optimizationConfig = require('./optimization.config');
 
 module.exports = function override(config) {
-  // Configurações de alias existentes
+  // Apply optimization settings
+  config = optimizationConfig.webpack.configure(config);
+
+  // Alias configuration
   config.resolve = {
     ...config.resolve,
     alias: {
-      ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
@@ -18,45 +21,6 @@ module.exports = function override(config) {
       '@themes': path.resolve(__dirname, 'src/themes'),
       '@config': path.resolve(__dirname, 'src/config'),
     },
-  };
-
-  // Otimizações agressivas
-  config.optimization = {
-    ...config.optimization,
-    minimize: true,
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-          name: 'vendor',
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-          name: 'common',
-        },
-      },
-    },
-  };
-
-  // Reduzir paralelismo do webpack
-  config.parallelism = 1;
-
-  // Cache do webpack
-  config.cache = {
-    type: 'filesystem',
-    compression: 'gzip',
-    cacheDirectory: path.resolve(__dirname, 'node_modules/.cache/webpack'),
   };
 
   return config;
