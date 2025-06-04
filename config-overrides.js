71f09f78abf31cd2f621cae35cf36c645c791b07
JonatasCarvalhoSousa <jonatas.carvalho.191098@outlook.com>
2025-06-04 16:20:23 -0300

    Deploy master 2.3

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = function override(config) {
+  // Configurações de alias existentes
   config.resolve = {
     ...config.resolve,
     alias: {
@@ -15,8 +16,32 @@ module.exports = function override(config) {
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@themes': path.resolve(__dirname, 'src/themes'),
-      '@config': path.resolve(__dirname, 'src/config')
-    }
+      '@config': path.resolve(__dirname, 'src/config'),
+    },
   };
+
+  // Otimizações para reduzir uso de memória
+  config.optimization = {
+    ...config.optimization,
+    moduleIds: 'deterministic',
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: 20,
+      maxAsyncRequests: 20,
+      minSize: 40000,
+    },
+  };
+
+  // Reduzir paralelismo do webpack para diminuir uso de memória
+  config.parallelism = 1;
+
+  // Habilitar cache do webpack
+  config.cache = {
+    type: 'filesystem',
+    compression: 'gzip',
+    cacheDirectory: path.resolve(__dirname, 'node_modules/.cache/webpack'),
+  };
+
   return config;
-}; 
+};
